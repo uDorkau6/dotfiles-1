8d614090867a6d070b601cf94e65835ab2757ee8
Jon Gold <4311322+uDorkau6@users.noreply.github.com>
2013-07-19 11:19:48 +0100

    Fix git & nvm config

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,4 @@
 export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
+export PATH="/Applications/Postgres.app/Contents/MacOS/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
