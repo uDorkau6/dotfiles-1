962daa7f1e763d1df3a974f1518b498cc8f31b06
Jon Gold <4311322+uDorkau6@users.noreply.github.com>
2013-08-02 14:38:40 +0100

    MORE PROMPTY STUFF

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -79,9 +79,9 @@ directory_name(){
   echo "%{$fg_bold[cyan]%}%1/%\/%{$reset_color%}"
 }
 
-export PROMPT=$'\n$(rb_prompt) in $(directory_name) $(git_dirty)$(need_push)\n› '
+export PROMPT=$'\n$(directory_name) $(git_dirty)$(need_push)\n➤  '
 set_prompt () {
-  export RPROMPT="%{$fg_bold[cyan]%}$(todo)%{$reset_color%}"
+  export RPROMPT="$(rb_prompt)%{$fg_bold[cyan]%}$(todo)%{$reset_color%}"
 }
 
 precmd() {
