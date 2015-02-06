03ec70ceec59c309d3a2a29ba1e037f37c32cf1c
Jon Gold <4311322+uDorkau6@users.noreply.github.com>
2015-02-06 14:03:37 +0000

    Move Postgres.app

--- system/path.zsh
+++ system/path.zsh
@@ -1,5 +1,5 @@
 export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
-export PATH="/Applications/Postgres.app/Contents/MacOS/bin:$PATH"
+export PATH="/Applications/Postgres.app/Contents/Versions/9.3/bin:$PATH"
 export PATH="$HOME/.dotfiles/vim:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
