ad0f059064f05cb10fc2c008e7f1dc1f626e3779
Jon Gold <4311322+uDorkau6@users.noreply.github.com>
2015-09-18 13:05:10 -0700

    Update pkgconfig path
    For Cairo? I think? Ugh

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,6 +1,10 @@
 export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
 export PATH="/Applications/Postgres.app/Contents/Versions/9.3/bin:$PATH"
 export PATH="$HOME/.dotfiles/vim:$PATH"
+export PATH="$PATH:$HOME/npm/bin"
+export PKG_CONFIG_PATH="/usr/local/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PKG_CONFIG_PATH="/opt/X11/lib/pkgconfig:$PKG_CONFIG_PATH"
+
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 
