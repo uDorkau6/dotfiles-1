7d2daffd9a783aaca6130f93410a4c17d2973252
Jon Gold <4311322+uDorkau6@users.noreply.github.com>
2015-09-18 13:03:15 -0700

    Update emacs init.el

--- emacs/emacs.d.symlink/init.el
+++ emacs/emacs.d.symlink/init.el
@@ -2,9 +2,11 @@
 
 ; List the packages you want
 (setq package-list '(cider
+         auto-complete
          base16-theme
          cider
          clojure-mode
+         ;; coffee-mode
          elscreen
          evil
          evil-leader
@@ -14,6 +16,7 @@
          ido-vertical-mode
          gist
          git-gutter
+         js2-mode
          linum-relative
          magit
          markdown-mode
@@ -21,7 +24,8 @@
          projectile
          rainbow-delimiters
          slime
-         smartparens))
+         smartparens
+         yasnippet))
 
 ; Add Melpa as the default Emacs Package repository
 ; only contains a very limited number of packages
@@ -57,12 +61,14 @@
 (evil-leader/set-key
   "r" 'recentf-ido-find-file
   "," 'projectile-find-file
+  "p" 'projectile-switch-project
   "c" 'comment-or-uncomment-region
   "b" 'switch-to-buffer
   "l" 'buffer-menu-other-window
   "w" 'save-buffer
   "k" 'kill-buffer
   "e" 'evil-buffer
+  "f" 'ido-find-file
   "q" 'eval-buffer)
 
 (require 'evil)
@@ -109,6 +115,7 @@
 ; General UI stuff
 (global-linum-mode t)
 (global-hl-line-mode t)
+(setq-default indent-tabs-mode nil)
 (setq default-tab-width 2)
 (setq inhibit-startup-message t)
 (setq visible-bell 'top-bottom)
@@ -128,12 +135,12 @@
 
 (require 'projectile)
 (projectile-global-mode)
-(setq projectile-enable-caching 0)
+;;(setq projectile-enable-caching 0)
 
 (require 'flx-ido)
-(require 'ido-vertical-mode)
+;; (require 'ido-vertical-mode)
 (ido-mode 1)
-(ido-vertical-mode 1)
+;; (ido-vertical-mode 0)
 (ido-everywhere 1)
 (flx-ido-mode 1)
 ;; disable ido faces to see flx highlights.
@@ -161,29 +168,82 @@
 (global-git-gutter-mode t)
 
 ;; Theme
-(load-theme 'base16-eighties t)
-(setq-default line-spacing 2)
-(set-default-font "Akkurat Mono-12")
+(load-theme 'base16-ocean t)
+(setq-default line-spacing 4)
+(set-default-font "Source Code Pro Light-13")
 
 (require 'powerline)
 (powerline-default-theme)
 
-(setq tab-always-indent 'complete)
-(add-to-list 'completion-styles 'initials t)
+;; (setq tab-always-indent 'complete)
+;; (add-to-list 'completion-styles 'initials t)
 
-;; only use spaces
-(setq-default indent-tabs-mode nil)
-(setq-default tab-width 2)
+;; coffeescript
+;; (custom-set-variables
+;;  '(coffee-tab-width 2)
+;;  '(coffee-args-compile '("-c" "--bare")))
+
+;; (eval-after-load "coffee-mode"
+;;   '(progn
+;;      (define-key coffee-mode-map [(meta r)] 'coffee-compile-buffer)
+;;      (define-key coffee-mode-map (kbd "C-j") 'coffee-newline-and-indent)))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("51bea7765ddaee2aac2983fac8099ec7d62dff47b708aa3595ad29899e9e9e44" default))))
+ '(custom-safe-themes (quote ("f0a99f53cbf7b004ba0c1760aa14fd70f2eabafe4e62a2b3cf5cabae8203113b" "978ff9496928cc94639cb1084004bf64235c5c7fb0cfbcc38a3871eb95fa88f6" "51bea7765ddaee2aac2983fac8099ec7d62dff47b708aa3595ad29899e9e9e44" default)))
+ '(org-agenda-files (quote ("~/Dropbox/org/personal.org" "~/Dropbox/org/yld.org" "~/Dropbox/Internet&Things/Talks/ftf.org"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(put 'erase-buffer 'disabled nil)
+
+;; org-mode
+(add-hook 'org-mode-hook 'turn-on-font-lock) ; not needed when global-font-lock-mode is on
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
+(setq org-todo-keywords
+      '((sequence "TODO"
+                  "IN-PROGRESS"
+                  "BLOCKED"
+                  "DONE")))
+
+;; Stop dialog boxes breaking
+(defadvice yes-or-no-p (around prevent-dialog activate)
+  "Prevent yes-or-no-p from activating a dialog"
+  (let ((use-dialog-box nil))
+    ad-do-it))
+(defadvice y-or-n-p (around prevent-dialog-yorn activate)
+  "Prevent y-or-n-p from activating a dialog"
+  (let ((use-dialog-box nil))
+    ad-do-it))
+;;; yasnippet
+;;; should be loaded before auto complete so that they can work together
+(require 'yasnippet)
+(yas-global-mode 1)
+
+;; auto complete mode
+;; should be loaded after yasnippet so that they can work together
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
+(ac-config-default)
+;; set the trigger key so that it can work together with yasnippet on tab key,
+;; if the word exists in yasnippet, pressing tab will cause yasnippet to
+;; activate, otherwise, auto-complete will
+(ac-set-trigger-key "TAB")
+(ac-set-trigger-key "<tab>")
+
+
+;; javascripting
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(add-hook 'js2-mode-hook 'ac-js2-mode)
+(setq js2-highlight-level 3)
+
+(defconst clojure--prettify-symbols-alist
+  '(("fn"  . ?λ)))
