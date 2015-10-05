f232694c7ff4a9cdd042bdc5a53eef2dd0326a16
Jon Gold <4311322+uDorkau6@users.noreply.github.com>
2015-10-05 10:49:18 -0700

    Update lein/profiles.clj

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,2 +1,5 @@
 {:user {:plugins [[lein-pprint "1.1.1"]
-                  [lein-try "0.4.1"]]}}
+                  [lein-midje "3.1.3"]
+                  [venantius/ultra "0.3.4"]
+                  [lein-try "0.4.1"]]
+        :ultra {:color-scheme :solarized_dark}}}
