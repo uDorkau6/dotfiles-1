101cc5f3331cfe5c195ccd26b9029cb24d33f2bb
Jon Gold <4311322+uDorkau6@users.noreply.github.com>
2015-10-05 10:49:42 -0700

    alias for focus.app

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -24,3 +24,6 @@ alias lf='ls -Gl | grep ^d' #Only list directories
 alias lsd='ls -Gal | grep ^d' #Only list directories, including hidden ones
 
 alias pcat="pygmentize -f terminal256 -O style=native -g"
+
+# Focus
+alias focus='open focus://toggle'
